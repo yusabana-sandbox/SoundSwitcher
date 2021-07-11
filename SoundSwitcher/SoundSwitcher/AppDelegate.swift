11a63813b49a25e5df19a491881dac0223b82bb8
Yuji Takaesu <taka.esu.user@gmail.com>
2021-07-12 06:05:17 +0900

    Add icon to menubar

--- SoundSwitcher/SoundSwitcher/AppDelegate.swift
+++ SoundSwitcher/SoundSwitcher/AppDelegate.swift
@@ -11,29 +11,27 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        
+        // statusItemにできてはいるが、アイコンが見えない(クリックはできる)
+        if let button = statusItem.button {
+          button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+          button.action = #selector(printQuote(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    @objc func printQuote(_ sender: Any?) {
+      let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+      let quoteAuthor = "Mark Twain"
+      
+      print("\(quoteText) — \(quoteAuthor)")
+    }
 
 }
 
