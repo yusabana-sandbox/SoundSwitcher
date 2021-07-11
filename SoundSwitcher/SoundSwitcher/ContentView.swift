11ba65ecc8f6d651e044716bb353d972a4dbc013
Yuji Takaesu <taka.esu.user@gmail.com>
2021-07-12 06:00:17 +0900

    Init xcode project

---
+++ SoundSwitcher/SoundSwitcher/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SoundSwitcher
+//
+//  Created by yuji-takaesu on 2021/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
